e362c2ad7e23db47efc147aca72bbee70c32ba32
ramakrishnas <ramakrishnas@gmail.com>
2016-03-16 12:38:07 -0500

    added awk read length

--- extract_flt3_reads.sh
+++ extract_flt3_reads.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 InputRun"
+  echo "Usage: $0 InputRun exon"
   exit 1
 fi
 InDir="$1"
-OutDir="$2"
+exon="$2"
 
 echo $InDir
-echo $OutDir
+echo $exon
 
 folder_tmp=${InDir/Auto_user_/}
 folder=${folder_tmp/AML_*/AML}
@@ -16,6 +16,7 @@ echo $folder_tmp
 echo $folder
 
 exon14_oligo='/Volumes/Pathology/Molecular Pathology/Active Validated Tests/Oncology/AML Panel/FLT3-ITD/flt3_ex14_oligo.fa'
+exon15_oligo='/Volumes/Pathology/Molecular Pathology/Active Validated Tests/Oncology/AML Panel/FLT3-ITD/flt3_ex15_oligo.fa'
 result_dir='/Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/'
 
 echo $result_dir$folder
@@ -33,7 +34,9 @@ for i in *.fastq; do
     usearch8.osx -search_oligodb $i -db /Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/flt3_ex14_oligo.fa \
     -strand both \
     -matched /Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/$folder/${i:0:13}_flt3_exon14.fa
- 
+ 	cat /Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/$folder/${i:0:13}_flt3_exon14.fa | \
+ 	awk '$0 ~ ">" {print c; c=0;printf substr($0,2,100) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }' | \
+ 	cut -f2 | sort -n | uniq -c > /Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/$folder/${i:0:13}_flt3_exon14_read_length.txt
 done
 
 #usearch8.osx -search_oligodb /Volumes/Iontorrent/Auto_user_MOL-370-AML_434_496/plugin_out/downloads/IonXpress_008.fastq -db /Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/flt3_ex14_oligo.fa -strand both -matched /Volumes/Pathology/Molecular\ Pathology/Active\ Validated\ Tests/Oncology/AML\ Panel/FLT3-ITD/R370/R370_bc008_flt3_exon14.fa &
